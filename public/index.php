09a56cec4e24c79606bbf84d455870132dfe1909
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-02-03 12:22:13 +0100

    public/index.php: Register Clockwork middleware

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 
 declare(strict_types=1);
 
+use Clockwork\DataSource\XdebugDataSource;
+use Clockwork\Storage\FileStorage;
+use Clockwork\Support\Psr\Middleware;
+use Clockwork\Support\Vanilla\Clockwork as VanillaClockwork;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\ResponseFactory;
 use Slim\Psr7\Factory\StreamFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/Middleware.php';
 
 /**
  * Instantiate App
@@ -24,6 +30,77 @@ $app = AppFactory::create();
  * This gives readable error messages in the browser.
  */
 $app->addErrorMiddleware(false, false, true);
+
+/**
+ * create response and stream factories
+ *
+ * These are used to generate HTTP responses. Normally, those would come from
+ * a DI container.
+ */
+$responseFactory = new ResponseFactory();
+$streamFactory = new StreamFactory();
+
+/**
+ * Storage Dir For Clockwork Profiles
+ *
+ * This must be a readable and writable directory. It's not necessary to
+ * create it up front though, it is created implicitly.
+ */
+$clockworkStorageDir = __DIR__ . '/storage/clockwork';
+
+/**
+ * Instantiate Clockwork Service Class
+ *
+ * More specifically, we use the "Vanilla" helper built on top of it. The
+ * helper specifically provides configuration handling,
+ */
+$clockwork = new class extends VanillaClockwork
+{
+	// TODO: All these methods here need to be integrated into the
+	// upstream VanillaClockwork.
+	public function isEnabled()
+	{
+		return $this->config['enable'];
+	}
+
+	public function getApiPath()
+	{
+		return $this->config['api'];
+	}
+
+	public function isWebEnabled()
+	{
+		return $this->config['web']['enable'];
+	}
+
+	public function getWebPath()
+	{
+		// TODO: Clarify what the meaning here is.
+		// return $this->config['web']['uri'];
+		return '/web';
+	}
+
+	public function isAuthenticationEnabled()
+	{
+		return $this->config['authentication'];
+	}
+};
+$clockwork->getClockwork()->addDataSource(new XdebugDataSource());
+$clockwork->getClockwork()->storage(new FileStorage($clockworkStorageDir));
+
+/**
+ * Install Clockwork middleware
+ *
+ * More specifically, we use the "Vanilla" helper built on top of it. The
+ * helper specifically provides configuration handling,
+ */
+$app->add(
+    new Middleware(
+        $clockwork,
+        $responseFactory,
+        $streamFactory
+    )
+);
 
 $app->get('/favicon.ico', function (Request $request, Response $response, array $args) {
     $streamFactory = new StreamFactory();
